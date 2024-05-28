fb3a28de66f9df41791caf133b614ca9caaafbd9
Yi Sun <yi-sun@users.noreply.github.com>
2024-05-28 17:04:11 -0400

    feat: enable usage of chains without pre-existing deployments in axiom-std (#25)
    * feat: enable usage of chains without pre-existing deployments in axiom-std
    
    * chore: update axiom-client
    
    * chore: update axiom-client
    
    * chore: fix CI

--- src/AxiomCli.sol
+++ src/AxiomCli.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.0;
 
 library AxiomCli {
     /// @dev The SHA256 hash of the Axiom CLI binary
-    bytes public constant CLI_SHASUM = hex"c9ebb0489e0c2db2b2b033a4ee0109af3e636090cfde44497fa0d6785f77db26";
+    bytes public constant CLI_SHASUM = hex"5ae07ff7d39f98884004eb6b0e0cb50cc311f092e92437dcf62d7c6a6ef0bb6d";
 }
